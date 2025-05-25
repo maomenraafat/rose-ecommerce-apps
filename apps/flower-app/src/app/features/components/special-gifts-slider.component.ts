719f0fa59d90b605df1f9d07876adfcd186b6324
Mo-Ashraf-Elsayed <muhamed.alfadli1@gmail.com>
2025-05-25 04:01:12 +0200

    spicial gifts feature

---
+++ apps/flower-app/src/app/features/components/special-gifts-slider.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OwlOptions, CarouselModule } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-special-gifts-slider',
+  imports: [CommonModule, CarouselModule],
+  templateUrl: './special-gifts-slider.component.html',
+  styleUrl: './special-gifts-slider.component.scss',
+})
+export class SpecialGiftsSliderComponent {
+  sliderStore: { imgPath: string; altText: string }[] = [
+    { imgPath: 'special-gifts1.jpg', altText: '' },
+    { imgPath: 'special-gifts-2.jpg', altText: '' },
+    { imgPath: 'special-gifts-3.jpg', altText: '' },
+    { imgPath: 'special-gifts-4.jpg', altText: '' },
+  ];
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: [
+      '<i class="fa-solid fa-angle-left"></i>',
+      '<i class="fa-solid fa-angle-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+    nav: true,
+  };
+}
