98866b32957018c933ba8d8ca38c6a7f795ff4ba
saad <saad>
2025-06-24 15:52:28 +0300

    Fix: Delete Daplication Component

--- apps/flower-app/src/app/app.component.ts
+++ apps/flower-app/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from './features/pages/home/home.component';
 
 @Component({
-  imports: [ RouterModule, HomeComponent],
+  imports: [ RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
